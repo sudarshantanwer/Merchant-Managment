09b82829c66a224b8676cebb8f19c72aa5728ab2
sudarshan <sudarshan@tothenew.com>
2018-01-20 19:44:57 +0530

    actions added to application

---
+++ src/actions.js
@@ -0,0 +1,19 @@
+/**
+ * Created by sudarshan on 17/1/18.
+ */
+import {MERCHANT_ACTIONS} from './constants';
+import {apis} from './apis/merchantApi'
+
+export const getMerchantList = (data, type)=>{
+    return {
+        type : MERCHANT_ACTIONS.GET_LIST,
+        data : apis.getMerchantList()     // In real this data will come from api call
+    }
+}
+
+export const deleteMerchant = (id)=>{
+    return {
+        type : MERCHANT_ACTIONS.DELETE_MERCHANT,
+        id : id     // In real this data will come from api call
+    }
+}
